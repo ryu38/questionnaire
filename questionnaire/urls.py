3ee42e4e88e8b1ef99e510406aff71ec9c5bdfa2
ryu38 <niceneetlife.ryu@gmail.com>
2020-03-17 18:56:03 +0900

    beta4

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += (
-        path('', include(debug_toolbar.urls)),
-    )
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += (
+#         path('', include(debug_toolbar.urls)),
+#     )
